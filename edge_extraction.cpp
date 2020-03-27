69881e1407a1c0e7da554ee1eb26def8c506a0fe
Dudododo <a752601260@gmail.com>
2020-03-27 15:02:46 +0800

    练习四

--- edge_extraction.cpp
+++ edge_extraction.cpp
@@ -15,10 +15,11 @@ int main()
 		cv::Mat src;
 		cv::Mat dstX;
 		cv::Mat dstY;
-
+		 
 		cap >> src;
-		cv::Sobel(src, dstX, CV_16SC1, 1, 0, 3); //x方向边缘
-		cv::Sobel(src, dstY, CV_16SC1, 0, 1, 3); //y方向边缘
+
+		cv::Sobel(src, dstX, CV_8U, 1, 0, 3, 1, 0, BORDER_DEFAULT); //x方向边缘
+		cv::Sobel(src, dstY, CV_8U, 0, 1, 3, 1, 0, BORDER_DEFAULT); //y方向边缘
 
 		cv::imshow("原图", src);
 		cv::imshow("x方向边缘", dstX);
